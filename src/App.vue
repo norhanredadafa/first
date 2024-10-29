950121d734c9bf3ff56251804cdbe990adb1de45
norhanredadafa <norhan.reda@dafagate.com>
2024-10-29 07:27:16 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import headerComp from './components/TheHeader.vue'
+</script>
+
+<template>
+  <div>
+    <headerComp />
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+
+export default {
+  components: {
+    RouterLink
+  }
+}
+</script>
+
+<style scoped></style>
