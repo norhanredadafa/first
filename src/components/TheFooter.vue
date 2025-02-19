f9047be109c23bcc0931d2ae343a32885167eb18
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2025-02-19 16:33:16 +0200

    change

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+      <footer class="">
+     
+      </footer>
+    </div>
+  </template>
+  
+  <script lang="ts">
+
+  
+  export default {
+    components: {
+      
+    },
+  }
+  </script>
+  
+  <style scoped>
+  /* You can add any additional styles here if needed */
+  </style>
+  
